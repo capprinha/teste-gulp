1fca57e8212e8f343fa9fc48a091c516af02c555
capprinha <gutoklein21@gmail.com>
2025-01-25 22:17:24 -0300

    melhora do js e sass

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,34 @@
-const btn = document.getElementById('btn');
-const conteudo = 'Sucesso!!';
-const span = document.getElementById('sucess')
+const meuBtn = document.getElementById('btn');
+const span = document.getElementById('sucess');
+const usuario = document.getElementById('nomeUsuario');
+const email = document.getElementById('email');
+const checkbox = document.getElementById('checkbox')
 
-if (btn){
-    btn.addEventListener('click', function(e){
+
+if(meuBtn){
+    meuBtn.addEventListener("click",function(e){
         e.preventDefault();
-        span.innerHTML = conteudo
-    })
-}
+        
+        if (usuario.value === '' && email.value === '' && checkbox.checked) {
+            span.innerHTML = 'Ambos os campos não estão preenchidos'
+        }
+        else if(usuario.value === '' ){
+            span.innerHTML = 'Usuario não preenchido'
+        }
+        else if(email.value === ''){
+            span.innerHTML = 'E-mail não preenchido'
+        }
+        else{
+            span.innerHTML = 'Sucesso!!'
+        }
+        if(checkbox){
+            if(checkbox.checked){
+                console.log('esta marcado')
+            }
+            else{
+                span.innerHTML = 'Aceite os ternos e condições'
+            }
+        
+        }
+    });
+};
